4688e38dd0793c326fcaeb56729d9924c0453db9
Demakov Nikita <demakoffnik@gmail.com>
2022-09-10 19:23:10 +0700

    add tasks

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from lessons.lesson2 import *
+from lessons.lesson3 import *
+
 # Press the green button in the gutter to run the script.
 
 if __name__ == '__main__':
-
-     analytics()
+   print(summa())
