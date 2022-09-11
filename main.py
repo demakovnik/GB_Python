ee84a785c2d6866a28f6c3ad1dc317e55d05fa39
Demakov Nikita <demakoffnik@gmail.com>
2022-09-11 13:00:54 +0700

    add tasks

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from lessons.lesson3 import *
 # Press the green button in the gutter to run the script.
 
 if __name__ == '__main__':
-   print(int_func('text', 'qwerty'))
+   print(int_func2('text qwerty'))
