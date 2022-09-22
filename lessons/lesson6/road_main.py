3f27d3e0cae52079e8ecb65c824ba921a3d1797b
Nikita Demakov <demakoffnik@gmail.com>
2022-09-22 16:19:53 +0700

    add Task2

---
+++ lessons/lesson6/road_main.py
@@ -0,0 +1,6 @@
+import Road
+
+if __name__ == "__main__":
+    road = Road.Road(20, 5000)
+
+    print(road.get_mass(5))
--- lessons/lesson6/traffic_light_main.py
+++ lessons/lesson6/traffic_light_main.py
@@ -1,5 +1,4 @@
 from lessons.lesson6 import TrafficLight
-from os import system
 
 if __name__ == "__main__":
     trafficLight = TrafficLight.TrafficLight()
