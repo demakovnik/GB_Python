40071216e8381efe3744d643f26bbca855b965c8
Nikita Demakov <demakoffnik@gmail.com>
2022-09-22 17:40:25 +0700

    add task 5

---
+++ lessons/lesson6/stationery_main.py
@@ -0,0 +1,9 @@
+import Stationery
+
+if __name__ == "__main__":
+    pen = Stationery.Pen()
+    pen.draw()
+    pencil = Stationery.Pencil()
+    pencil.draw()
+    handle = Stationery.Handle()
+    handle.draw()
